5f4f69b6071e3df6ec6097c908dcec5129221eb8
M Aghead Albakhe <aghyadalbalkhi@gmail.com>
2021-03-21 23:49:02 +0200

    use Card inested of Media and use Events Handlling

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -8,8 +8,36 @@ class Menu extends Component {
         super(props);
 
         this.state = {
+            selectedDish:null,
 
         };
+    }
+
+    onDishSelect(dish){
+
+        this.setState({selectedDish:dish})
+    }
+
+    renderDish(dish){
+
+        if(dish !=null){
+
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>  
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+
+        }else{
+
+            return(
+                <div> </div>
+            )
+        }
     }
 
     render(){
@@ -17,16 +45,13 @@ class Menu extends Component {
         const menu =this.props.dishes.map( (dish) => {
 
             return(
-                <div key ={dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={dish.image} alt={dish.name} />
-                        </Media>
-                        <Media body className="ml-5">
-                            <Media heading >{dish.name}</Media>
-                            <p>{dish.description}</p>
-                        </Media>
-                    </Media>
+                <div key ={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={()=> this.onDishSelect(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             );
 
@@ -36,10 +61,11 @@ class Menu extends Component {
             <div className="container">
 
                 <div className="row">
-                    <Media list>
-                        {menu}
-                    </Media>
+                    {menu}
+                </div>
 
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
             </>
