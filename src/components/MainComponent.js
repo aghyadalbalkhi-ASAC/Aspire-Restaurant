b71c9fad0e22c9725617597bb8d0c3cf3cdbda18
aghyad albalkhi <aghyadalbalkhi@gmail.com>
2021-04-05 23:48:15 +0300

    Header and Footer Added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,10 +2,11 @@
 // that related to Menu featuer and its also responsble of passing the data for theses Components which
 // called Presentional Components 
 
-import React,{ Component } from 'react';
-import {Navbar,NavbarBrand} from 'reactstrap'           //Bootstrap Moduel 
+import React,{ Component } from 'react';        //Bootstrap Moduel 
 import Menu from './MenuComponent';                     // Presentional Component
 import Dishdetail from './DishdetailComponent'          // Presentional Component
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 // Import Dishes Data from dishes Component 
 import { DISHES } from '../shared/dishes';              // Data Shared Component
 
@@ -30,12 +31,8 @@ class Main extends Component {
         return (
             <>
                 <div>
-                {/* Nav Bar using reactstrap */}
-                <Navbar dark color="primary">
-                    <div className="container">
-                    <NavbarBrand href="/"> Aspire Restaurant </NavbarBrand>
-                    </div>
-                </Navbar>
+
+                <Header />
             
                 {/* Render The Menu  Component and pass the dishes array as a props*/}
                 {/* passing a fuctional varible as a props [onClick] and use this varible on child on
@@ -44,6 +41,7 @@ class Main extends Component {
 
                 {/* To understand this we can change the name of onClick Props for anything like 'vv' and
                 use 'vv' props in child and it will work perfect */}
+                
                 <Menu dishes={this.state.dishes}
                 onClick={(dishId)=> this.onDishSelect(dishId)} />
 
@@ -52,6 +50,7 @@ class Main extends Component {
                 onDishSelect fucttion onClick Event*/}
                 
                 <Dishdetail dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0]} />
+                <Footer />
                 </div>
             </>
     );
